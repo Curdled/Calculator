8cc36b3468ad9c9b246855c4c4e00f5d186ab5c1
Joe Isaacs <joe.isaacs@live.co.uk>
2015-08-23 11:19:39 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Joe Isaacs on 23/08/2015.
+//  Copyright (c) 2015 Joe Isaacs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
